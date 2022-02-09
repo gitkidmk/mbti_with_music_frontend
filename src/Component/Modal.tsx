e452e990178f4d60a5a32cd4fb049809554e155f
강민경 <minkyeong_kang@tmax.co.kr>
2022-02-09 17:46:03 +0900

    [chore] add Modal component
    - modal 생성
    - error 생성중
    - image 추가

--- src/Component/Modal.tsx
+++ src/Component/Modal.tsx
@@ -1,25 +1,59 @@
 import styled from "styled-components";
 import great from "Asset/image/great.png";
 
-const Modal = () => {
+const Modal = ({ music_name }: any) => {
   return (
     <ModalOuterBox>
       <ModalBox>
         <ThumbsUpImg alt="thumbs-up" src={great} />
-        <MessageBox></MessageBox>
-        <CloseButton></CloseButton>
+        <MessageBox>
+          <MusicName>{music_name}</MusicName> 노래를 이미 추천했어요!
+        </MessageBox>
+        <CloseButton>OK</CloseButton>
       </ModalBox>
     </ModalOuterBox>
   );
 };
 
-const ModalOuterBox = styled.div``;
+const ModalOuterBox = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #ffffff9e;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
-const ModalBox = styled.div``;
+const ModalBox = styled.div`
+  width: 90%;
+  height: 200px;
+  background: rgba(249, 242, 203, 0.651);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+`;
 
-const ThumbsUpImg = styled.img``;
+const ThumbsUpImg = styled.img`
+  width: 100px;
+  margin-bottom: 20px;
+`;
 
-const MessageBox = styled.div``;
+const MessageBox = styled.div`
+  margin-bottom: 10px;
+`;
 
-const CloseButton = styled.button``;
+const MusicName = styled.span`
+  font-weight: bold;
+`;
+
+const CloseButton = styled.button`
+  background-color: rgb(2, 90, 77);
+  border-radius: 10px;
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+`;
 export default Modal;
